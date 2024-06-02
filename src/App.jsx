402178f44441c4d22d2d6f4249a38ac9cb0026e8
tlgiao <Giao2210@>
2024-06-02 20:53:56 +0700

    chore: Select a size for the template

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import MainComponent from "./Exercise-Component/MainComponent";
+import Glasses from "./Exercise-Glasses/Glasses";
+
+function App() {
+  return (
+    <>
+      {/* Exercise 1 */}
+      {/* <MainComponent /> */}
+
+      {/* Exercise 2 */}
+      <Glasses />
+    </>
+  );
+}
+
+export default App;
